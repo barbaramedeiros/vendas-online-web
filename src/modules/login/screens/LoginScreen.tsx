39f8f507bd565f2639033619ae2f390ea717ebf8
Barbara Medeiros <barbmedeiros5@gmail.com>
2024-10-22 23:14:45 -0300

    update

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,18 +1,25 @@
+import Input from "../../../shared/inputs/input/input"
 import { BackgroundImage, ContainerLoginScreen } from "../styles/loginScreen.styles";
 import { ContainerLogin } from "../styles/loginScreen.styles";
 import { LogoImage } from "../styles/loginScreen.styles";
 import { LimitedContainer } from "../styles/loginScreen.styles";
+
+
 const LoginScreen = () => {
 
     return (
-        <ContainerLoginScreen>
-            <BackgroundImage src=".\public\background.png" />
-            <LimitedContainer>
-                <ContainerLogin>
-                    <LogoImage src=".\public\logo.jpg" />
-                </ContainerLogin>
-            </LimitedContainer>
-        </ContainerLoginScreen>
+        <div>
+            <ContainerLoginScreen>
+                <BackgroundImage src=".\public\background.png" />
+                <LimitedContainer>
+                    <ContainerLogin>
+                        <LogoImage src=".\public\logo.jpg" />
+                        <Input title="USUARIO"  />
+                        <Input title="SENHA" />
+                    </ContainerLogin>
+                </LimitedContainer>
+            </ContainerLoginScreen>
+        </div>
     );
 };
 
--- src/modules/login/styles/loginScreen.styles.ts
+++ src/modules/login/styles/loginScreen.styles.ts
@@ -14,33 +14,28 @@ export const ContainerLogin = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    flex-direction:column;
     background-color: #d9d9d9;
     width: 100%;
     height: 100vh;
     max-width: 646px;
     position: absolute;
-    right: 0;
-    top: 0;
-    z-index: 2;
-    padding: 22px;
+    z-index:2;
+    
 `;
 
 export const LogoImage = styled.img``;
 
 export const LimitedContainer = styled.div`
     display:flex;
-    align-items: center;
     justify-content: center;
     width:100%;
     max-width: 498px;
     
-
 `;
 
 export const ContainerLoginScreen = styled.div`
     width:100%;
     display: flex;
     justify-content: right;
-
-
-`
+`;
